92a17db95638a069e95f3f525e177003dbc2924f
komisiyonericonnectproservices-stack <komisiyonericonnectproservices@gmail.com>
2026-04-26 15:39:25 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-storage-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAtNnbsXKFgABvP-nTvGwz6pooGnSWSshY",
-  authDomain: "komisiyoneri-78e82.firebaseapp.com",
-  projectId: "komisiyoneri-78e82",
-  storageBucket: "komisiyoneri-78e82.firebasestorage.app",
-  messagingSenderId: "580559336922",
-  appId: "1:580559336922:web:e115d37c1c0149d8c0fce3",
-  measurementId: "G-ZTVZQ5W9Y0"
-};
+<script>
+  // Iyi ni ya kode yawe wanyeretse, ariko yanditse neza ngo ikore muri Browser
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtNnbsXKFgABvP-nTvGwz6pooGnSWSshY",
+    authDomain: "komisiyoneri-78e82.firebaseapp.com",
+    projectId: "komisiyoneri-78e82",
+    storageBucket: "komisiyoneri-78e82.firebasestorage.app",
+    messagingSenderId: "580559336922",
+    appId: "1:580559336922:web:2275a3013b07cf56c0fce3",
+    measurementId: "G-NE0PHMB885"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Tangiza Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Tangiza Database na Storage (Ibi ni ingenzi cyane)
+  const db = firebase.firestore();
+  const storage = firebase.storage();
+</script>
