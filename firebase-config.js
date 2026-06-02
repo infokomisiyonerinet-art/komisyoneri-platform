2e9d25f63dfd67f885488d80726f464115fd467f
infokomisiyonerinet-art <info.komisiyoneri.net@gmail.com>
2026-06-03 00:59:09 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,21 @@
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js"></script>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-<script>
-// Iyi ni ya kode yawe wanyeretse, ariko yanditse neza ngo ikore
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtNnbsXKFgABvP-nTvGwz6pooGnSWsshY",
-  authDomain: "komisiyoneri-78e82.firebaseapp.com",
-  databaseURL: "https://komisiyoneri-78e82-default-rtdb.firebaseio.com",
-  projectId: "komisiyoneri-78e82",
-  storageBucket: "komisiyoneri-78e82.firebasestorage.app",
-  messagingSenderId: "580559336922",
-  appId: "1:580559336922:web:2275a3013b07cf56c0fce3",
-  measurementId: "G-NEOPHMB885"
+  apiKey: "AIzaSyCw9NYlw0XLC26Di-nFCNOuL7D6RX8k820",
+  authDomain: "komisyoneri-platform-prod.firebaseapp.com",
+  projectId: "komisyoneri-platform-prod",
+  storageBucket: "komisyoneri-platform-prod.firebasestorage.app",
+  messagingSenderId: "766901928352",
+  appId: "1:766901928352:web:9df910b36a462e1fb524c5",
+  measurementId: "G-ERRNCE85E2"
 };
 
-// Tangiza Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Tangiza Database na Firestore
-const db = firebase.firestore();
-const rtdb = firebase.database();
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
